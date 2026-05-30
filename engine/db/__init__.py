f8ee884fa48189f3ed32a003e94fd1aeddf0492a
Nicolas Janssen <njanss2@wgu.edu>
2026-05-30 08:37:26 -0400

    AW-103: SQLAlchemy ORM models for all platform tables (Closes #5)

---
+++ engine/db/__init__.py
@@ -0,0 +1,39 @@
+"""Database ORM models for Arcwright persistence."""
+
+from .orm import (
+    Account,
+    ArcBeatState,
+    Base,
+    Character,
+    ConsentRecord,
+    Decision,
+    DecisionLog,
+    Event,
+    Fact,
+    GenerationLog,
+    KnowledgeState,
+    Location,
+    Object,
+    RelationshipState,
+    Session,
+    SessionParticipant,
+)
+
+__all__ = [
+    "Account",
+    "ArcBeatState",
+    "Base",
+    "Character",
+    "ConsentRecord",
+    "Decision",
+    "DecisionLog",
+    "Event",
+    "Fact",
+    "GenerationLog",
+    "KnowledgeState",
+    "Location",
+    "Object",
+    "RelationshipState",
+    "Session",
+    "SessionParticipant",
+]
